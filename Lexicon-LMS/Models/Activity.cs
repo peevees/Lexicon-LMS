06b2fb8701e5d0c3da656d8d34b8696e1a436a92
rdubrovskis <38496422+rdubrovskis@users.noreply.github.com>
2018-07-18 16:32:40 +0200

    Future Models added with properties
    * Add base files for future Models with Properties

---
+++ Lexicon-LMS/Models/Activity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lexicon_LMS.Models
+{
+    public class Activity
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime Deadline { get; set; }
+        public ICollection<Document> Documents { get; set; }
+    }
+
+    public class Assignment : Activity
+    {
+
+    }
+}
