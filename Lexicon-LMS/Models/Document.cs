db11a0180ca279e6000852e474689666292ce989
rdubrovskis <38496422+rdubrovskis@users.noreply.github.com>
2018-07-18 16:32:40 +0200

    Future Models added with properties
    * Add base files for future Models with Properties

---
+++ Lexicon-LMS/Models/Document.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace Lexicon_LMS.Models
+{
+    public class Document
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Filepath { get; set; }
+    }
+}
---
+++ Lexicon-LMS/Models/Notification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lexicon_LMS.Models
+{
+    public class Notification
+    {
+        public int ID { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public IEnumerable<ApplicationUser> Recipients { get; set; }
+        public ApplicationUser Sender { get; set; }
+        public Document Attachment { get; set; }
+        public bool Read { get; set; } = false;
+    }
+
+}
