ca769fca768ed4702ad9179d79c2ee52720cc243
Robert Dubrovskis <robert.dubrovskis@gmail.com>
2018-07-19 10:17:13 +0200

    Deleted seed

--- Lexicon-LMS/Migrations/Configuration.cs
+++ Lexicon-LMS/Migrations/Configuration.cs
@@ -20,37 +20,7 @@ namespace Lexicon_LMS.Migrations
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
 
-            context.Courses.AddOrUpdate(
-                c => c.CourseName,
-                new Course
-                {
-                    CourseName = ".NET Development",
-                    StartDate = new DateTime(2018, 7, 19),
-                    EndDate = new DateTime(2019, 7, 19),
-                    Description = "A course in .NET Development",
-                    CourseCode = "DN-18"
-                }
-                );
-
-            context.Modules.AddOrUpdate(
-                m => m.Description,
-                new Module
-                {
-                    Course = context.Courses.Where(c => c.CourseCode == "DN-18").FirstOrDefault(),
-                    Description = "Basics of C#",
-                    StartDate = new DateTime(2018, 7, 19),
-                    EndDate = new DateTime(2018,8,6)
-                }
-                );
-
-            context.Activities.AddOrUpdate(
-                a => a.Name,
-                new Activity
-                {
-                    Name = "Hello World!",
-                    Deadline = new DateTime(2018,7,20)
-                }
-                );
+         
         }
     }
 }
