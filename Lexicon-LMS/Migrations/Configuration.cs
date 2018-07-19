3f8c8b3b39fe6e46d668c70936d589d50bfa18bf
Mike <peevees@users.noreply.github.com>
2018-07-19 09:11:19 +0200

    Add seed for Roles Add Role Teacher

--- Lexicon-LMS/Migrations/Configuration.cs
+++ Lexicon-LMS/Migrations/Configuration.cs
@@ -22,6 +22,26 @@ namespace Lexicon_LMS.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+            var roleNames = new[] { "Teacher" };//REMINDER: add roles here!
+            AddRoles(context, roleNames);
+        }
+
+        private void AddRoles(ApplicationDbContext db, string[] roles)
+        {
+            var roleStore = new RoleStore<IdentityRole>(db);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+
+            foreach (var roleName in roles)
+            {
+                if (db.Roles.Any(i => i.Name == roleName)) continue;
+                var role = new IdentityRole { Name = roleName };
+                var result = roleManager.Create(role);
+                if (!result.Succeeded)
+                {
+                    throw new Exception(string.Join("/n", result.Errors));
+                }
+            }
         }
     }
 }
