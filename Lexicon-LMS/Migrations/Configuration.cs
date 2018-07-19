1e680aad05b0500e297984f351059d296c6b1688
Mike <peevees@users.noreply.github.com>
2018-07-19 09:20:48 +0200

    Add seed for users Add 1 Teacher and 1 Student

--- Lexicon-LMS/Migrations/Configuration.cs
+++ Lexicon-LMS/Migrations/Configuration.cs
@@ -24,6 +24,9 @@ namespace Lexicon_LMS.Migrations
             //  to avoid creating duplicate seed data.
             var roleNames = new[] { "Teacher" };//REMINDER: add roles here!
             AddRoles(context, roleNames);
+
+            var users = new[] { "teacher@shit.se", "student@shit.se" };//REMINDER: add users here!
+            AddUsers(context, users);
         }
 
         private void AddRoles(ApplicationDbContext db, string[] roles)
@@ -42,6 +45,25 @@ namespace Lexicon_LMS.Migrations
                     throw new Exception(string.Join("/n", result.Errors));
                 }
             }
+        }
+
+        private void AddUsers(ApplicationDbContext db, string[] usersEmail)
+        {
+            var userStore = new UserStore<ApplicationUser>(db);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+
+            foreach (var userEmail in usersEmail)
+            {
+                if (db.Users.Any(u => u.UserName == userEmail)) continue;
+                var user = new ApplicationUser { UserName = userEmail, Email = userEmail };
+                var result = userManager.Create(user, "P@$$w0rd");
+                if (!result.Succeeded)
+                {
+                    throw new Exception(string.Join("/n", result.Errors));
+                }
+            }
+            var teacherUser = userManager.FindByName("teacher@shit.se");
+            userManager.AddToRole(teacherUser.Id, "Teacher");
         }
     }
 }
