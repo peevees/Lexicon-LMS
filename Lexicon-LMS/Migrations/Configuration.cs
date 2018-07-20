a651c6f381a947e9017cdcba8ec1bac7bd753d42
rdubrovskis <38496422+rdubrovskis@users.noreply.github.com>
2018-07-20 13:34:43 +0200

    Models (#9)
    * Added base files for future Models
    
    * Added Properties to Models
    
    * Created Controllers and Views for the Models
    
    * Added a migration for the Models
    
    * Renamed migration
    
    * Added seeding for Course, Module and Activity
    
    * Deleted seed
    
    * Added Course, Module, Activity seed
    
    * Added functionality to Module and Course creation
    
    * Added Foreign Key declaration to Module model
    
    * Removed foreign key declaration
    
    * Added explicit Foreign Key declaration to Module, again.
    
    * Added CourseID to seed operation
    
    * Changes to Modules, Courses and Activities as well as their database properties
    
    * Changes to Model creation
    
    * change seed
    
    * WEb
    
    * aaa
    
    * aaaaa
    
    * aa
    
    * qq

--- Lexicon-LMS/Migrations/Configuration.cs
+++ Lexicon-LMS/Migrations/Configuration.cs
@@ -22,6 +22,11 @@ namespace Lexicon_LMS.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+
+            //TODO: SEED DEBUGGER
+            //if (!System.Diagnostics.Debugger.IsAttached)
+            //    System.Diagnostics.Debugger.Launch();
+
             var roleNames = new[] { "Teacher" };//REMINDER: add roles here!
             AddRoles(context, roleNames);
 
@@ -35,7 +40,7 @@ namespace Lexicon_LMS.Migrations
         {
             var roleStore = new RoleStore<IdentityRole>(db);
             var roleManager = new RoleManager<IdentityRole>(roleStore);
-
+            
             foreach (var roleName in roles)
             {
                 if (db.Roles.Any(i => i.Name == roleName)) continue;
@@ -56,7 +61,7 @@ namespace Lexicon_LMS.Migrations
             foreach (var userEmail in usersEmail)
             {
                 if (db.Users.Any(u => u.UserName == userEmail)) continue;
-                var user = new ApplicationUser { UserName = userEmail, Email = userEmail, TimeOfRegistration = new DateTime(2000, 01, 01, 00, 00, 00), EmailConfirmed = true };
+                var user = new ApplicationUser { UserName = userEmail, Email = userEmail, TimeOfRegistration = new DateTime(2000, 01, 01, 00, 00, 00) };
                 var result = userManager.Create(user, "P@$$w0rd");
                 if (!result.Succeeded)
                 {
@@ -78,29 +83,38 @@ namespace Lexicon_LMS.Migrations
                     EndDate = new DateTime(2019, 7, 19),
                     Description = "A course in .NET Development",
                     CourseCode = "DN-18"
-                }
-                );
+                });
 
-            //context.Modules.AddOrUpdate(
-            //    m => m.Description,
-            //    new Module
-            //    {
-            //        Course = context.Courses.Where(c => c.CourseCode == "DN-18").FirstOrDefault(),
-            //        CourseID = context.Courses.Where(c => c.CourseCode == "DN-18").FirstOrDefault().ID,
-            //        Description = "Basics of C#",
-            //        StartDate = new DateTime(2018, 7, 19),
-            //        EndDate = new DateTime(2018, 8, 6)
-            //    }
-            //    );
+            context.SaveChanges();
+            Course seededCourse = context.Courses.Where(c => c.CourseCode == "DN-18").FirstOrDefault();
 
-            //context.Activities.AddOrUpdate(
-            //    a => a.Name,
-            //    new Activity
-            //    {
-            //        Name = "Hello World!",
-            //        Deadline = new DateTime(2018, 7, 20)
-            //    }
-            //    );
+            context.Modules.AddOrUpdate(
+                m => m.Description,
+                new Module
+                {
+                    Description = "C# Basics",
+                    StartDate = new DateTime(2018, 7, 19),
+                    EndDate = new DateTime(2018, 7, 31),
+                    Course = seededCourse,
+                    CourseID = seededCourse.ID
+                });
+
+            context.SaveChanges();
+            Module seededModule = context.Modules.Where(c => c.CourseID == seededCourse.ID).FirstOrDefault();
+            seededCourse.CourseModules.Add(seededModule);
+
+            context.Activities.AddOrUpdate(
+                a => a.Name,
+                new Activity
+                {
+                    Name = "Hello World!",
+                    Deadline = new DateTime(2018, 7, 20),
+                    Module = seededModule,
+                    ModuleID = seededModule.ID
+                });
+            context.SaveChanges();
+            Activity seededActivity = context.Activities.Where(c => c.ModuleID == seededModule.ID).FirstOrDefault();
+            seededModule.ModuleActivities.Add(seededActivity);
         }
     }
 }
