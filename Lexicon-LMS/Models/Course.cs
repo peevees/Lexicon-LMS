ce360a62d4874d3983c5f40c431fd1b5f51be2fe
Fredrik <blocket_konto@hotmail.se>
2018-07-24 11:40:45 +0200

    Add some more validation. Undone.

--- Lexicon-LMS/Models/Course.cs
+++ Lexicon-LMS/Models/Course.cs
@@ -11,13 +11,10 @@ namespace Lexicon_LMS.Models
     {
         public int ID { get; set; }
 
-        //Course code regex: ^\s*([A-Z]{2}-[0-9]{2})\s*$
-
-        //[Required(ErrorMessage = "The email address is required")]
-        //[EmailAddress(ErrorMessage = "Invalid Email Address")]
-
         [Required(ErrorMessage ="Course code is required")]
         [Display(Name = "Course code")]
+        [RegularExpression("^\\s*([A-Z]{2}-[0-9]{2})\\s*$",
+            ErrorMessage ="The course code need to be in the format two capital letters, a dash and two numbers. For example:\"NG-18\"")]
         public string CourseCode { get; set; }
 
         [Required(ErrorMessage = "Course name is required")]
