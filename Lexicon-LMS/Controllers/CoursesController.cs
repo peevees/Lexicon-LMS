d59a40b7574f4741d3879543adb120ee50386da9
jesper.furtenbach <jesper.furtenbach@gmail.com>
2018-07-24 10:48:43 +0200

    add Course Layout.

--- Lexicon-LMS/Controllers/CoursesController.cs
+++ Lexicon-LMS/Controllers/CoursesController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using Lexicon_LMS.Models;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using Lexicon_LMS.Models;
 
 namespace Lexicon_LMS.Controllers
 {
