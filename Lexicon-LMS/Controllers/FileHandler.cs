23e2c0444c4518b65bd9633ab7d3870a939190e2
stekman08 <38240615+stekman08@users.noreply.github.com>
2018-08-01 09:39:55 +0200

    File handling again (#33)
    * Add required on notification, activity, module, course
    Add correction of name convention
    
    * fix configuration seed module null exception
    
    * Remove 2FactorAuth from ProfileInfo
    Change PostCode to Postcode
    
    * Add [Display(Name = "Registrated since")]] to TimeOfRegistration.
    
    * Add regex example in comment
    
    * Add validation. Undone.
    
    * Add some more validation. Undone.
    
    * Add RedirectToAction if not teacher.
    Add small layout fix.
    
    * Add Archive views and controller
    
    * Add Archive views and controller
    
    * Add sitewide css
    
    * Add back link in forgot password
    
    * Add active courses button with funciton in archived courses.
    Add active courses only display active courses not archived also.
    
    * Add file upload not done check file path extra filename
    
    * add comment
    
    * Add file download
    
    * Add file handling to courses and modules (create and detail view)
    
    * Add upload file to activity (not working yet)
    
    * Add still not working (upload file activity)
    
    * Add working upload and download to modules and activities (create and detail), fix
    
    * Filehandler again

---
+++ Lexicon-LMS/Controllers/FileHandler.cs
@@ -0,0 +1,95 @@
+﻿using Lexicon_LMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Web;
+using System.IO;
+using System.Reflection;
+
+namespace Lexicon_LMS.Controllers
+{
+    public class FileHandler : Controller
+    {
+
+        public Document UploadFile (HttpPostedFileBase upload)
+        {
+            if (upload != null && upload.ContentLength > 0)
+            {
+                var originalFilename = Path.GetFileName(upload.FileName);
+                //var user = db.Users.Where(u => u.UserName == User.Identity.Name).FirstOrDefault();
+
+                string fileId = Guid.NewGuid().ToString().Replace("-", "");
+
+                var fileName = fileId + "_" + originalFilename;
+
+                var path = Path.Combine(System.Web.HttpContext.Current.Server.MapPath("~/Uploads"));
+                var save = Path.Combine(System.Web.HttpContext.Current.Server.MapPath("~/Uploads"), fileName);
+                upload.SaveAs(save);
+
+                var file = new Document
+                {
+                    FileName = fileName,
+                    DisplayName = originalFilename,
+                    UploadDate = DateTime.Now,
+                    //CourseID = course.ID,
+                    Filepath = path,
+                    //User = user
+                };
+
+                return file;
+            }
+
+            return null;
+        }
+
+        public FileContentResult DownloadFile (string filePath, string fileName)
+        {
+            string fullName = Path.Combine(Assembly.GetExecutingAssembly().CodeBase, filePath, fileName);
+
+            if (!System.IO.File.Exists(fullName))
+            {
+                return null;
+            }
+
+            string contentType = MimeMapping.GetMimeMapping(filePath);
+            byte[] fileBytes = GetFile(fullName);
+
+            fileName = fileName.Substring(fileName.IndexOf("_"));
+
+            var cd = new System.Net.Mime.ContentDisposition
+            {
+                FileName = fileName,
+                Inline = false
+            };
+
+            System.Web.HttpContext.Current.Response.AppendHeader("Content-Disposition", cd.ToString());
+            return File(
+             fileBytes,
+             contentType
+             );
+        }
+
+        private byte[] GetFile(string fullName)
+        {
+
+            //is null check filepath
+            //https://stackoverflow.com/questions/3597179/file-download-in-asp-net-mvc-2
+
+            FileStream fs = System.IO.File.OpenRead(fullName);
+
+            byte[] data = new byte[fs.Length];
+            int br = fs.Read(data, 0, data.Length);
+            if (br != fs.Length)
+                throw new IOException(fullName);
+
+            return data;
+        }
+    }
+
+   
+}
