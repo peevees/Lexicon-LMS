e3fdc119af16986be036cdca09b05e00064d7617
Peevees <peevees@users.noreply.github.com>
2018-08-01 11:21:53 +0200

    fix created user is not automatically logged in

--- Lexicon-LMS/Controllers/AccountController.cs
+++ Lexicon-LMS/Controllers/AccountController.cs
@@ -190,7 +190,7 @@ namespace Lexicon_LMS.Controllers
                     Course userCourse = db.Courses.Where(c => c.CourseCode == user.UserCourseCode).FirstOrDefault();
                     userCourse.CourseParticipants.Add(user);
 
-                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                    //await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
