c0f2e81fa52bbbffca2b50c523bdeda9b3187bbe
Peevees <peevees@users.noreply.github.com>
2018-08-02 14:37:49 +0200

    FIX date format fix Course detail back to courses button

--- Lexicon-LMS/Models/Document.cs
+++ Lexicon-LMS/Models/Document.cs
@@ -13,9 +13,11 @@ namespace Lexicon_LMS.Models
         public string FileName { get; set; }
         public string DisplayName { get; set; }
 
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Upload date")]
         public DateTime UploadDate { get; set; }
-        
+
         public string Filepath { get; set; }
         public int? CourseID { get; set; }
         public int? ModuleID { get; set; }
