0a47ddba8910da7119d714deba56202e5f34fc2f
rdubrovskis <38496422+rdubrovskis@users.noreply.github.com>
2018-08-03 13:13:45 +0200

    Robert d (#37)
    * Added base files for future Models
    
    * Added Properties to Models
    
    * Created Controllers and Views for the Models
    
    * Added a migration for the Models
    
    * Renamed migration
    
    * Added seeding for Course, Module and Activity
    
    * Deleted seed
    
    * Added Course, Module, Activity seed
    
    * Added functionality to Module and Course creation
    
    * Added Foreign Key declaration to Module model
    
    * Removed foreign key declaration
    
    * Added explicit Foreign Key declaration to Module, again.
    
    * Added CourseID to seed operation
    
    * Changes to Modules, Courses and Activities as well as their database properties
    
    * Changes to Model creation
    
    * change seed
    
    * WEb
    
    * aaa
    
    * aaaaa
    
    * aa
    
    * qq
    
    * Added functionality for editing user's own personal info, edited user's own profile view to show personal info.
    
    * If a user is logged in and has a Forename in database, the welcome message will show name instead of e-mail.
    
    Added ability to assign course on User registration.
    Added ability for Teachers to edit a user's course.
    Added ability to assign a Teacher when editing or creating a Course.
    
    Added Course and CourseCode to Account and Identity models.
    Added Teacher ID to Course model.
    
    * added authorizations to CoursesController
    
    * h
    
    * ol
    
    * aaa
    
    * modified seed operations to add student to .NET course participants and assign teacher to .NET course
    
    * Added a UserProfile view that lets Users see a Details view of another User
    
    -UserProfile view is only viewable if the active User is a Teacher or is part of the same Course.
    -If the active User accesses their own UserProfile view they are redirected to Manage/Index view instead.
    -If the active User is not a Teacher, only basic information is visible in a UserProfile view
    -If the active User is a Teacher and there is an option to edit the UserProfile.
    
    Modified Course/Details view so that Teacher and Student listings have an ActionLink to their respective UserProfile views.
    
    * Changed Course display name to "Course" and set it as a Required value
    
    * Added Notifications functionalities
    
    * Added notifications and notification inbox
    
    * aaa
    
    * Added a migration for after amster merge
    Fixed Notification sorting in Inbox
    
    * Added "Compose new message" button to inbox
    Changed the return link in Notifications/Create to return to inbox if accessed from inbox
    Changed the redirect after POST in Notifications/Create to redirect back to inbox
    
    * small changes to inbox container layout
    
    * FIX readd  all missing migrations
    
    * Edited the migrations
    
    * Notifications migration
    
    * Changed the method of editing a User entry in EditProfile action in ManageController
    
    * Role checkbox for Register view added.
    ActionLink to Register in Course Participants now auto-selects the appropriate course in form.
    
    Removed role authorization for creating Notifications
    
    * Fixed syntax error in _ModulesList
    Changed EditProfile action in ManageController to not break when course is already assigned but was unchanged.
    
    * Fixed EditProfile when moving from one Course to another
    
    * Added Inbox tab to dropdown in the header
    
    * Fixed the crash when editing activity
    
    * Fixed crash on viewing another user's details as Student.
    Fixed crash on attempting to edit an archived Course.
    
    * *Moved recurring URL query parser to a separate file (Scripts/UserScripts.js)
    *Added function to Activities controller to optionally notify all course participants when a new activity is added.
    *If a new User is registered from Courses/Details views the form will redirect back to course participants tab.
    
    * fixed Acitivity creation and Course Details crashing

---
+++ Lexicon-LMS/Scripts/UserScripts.js
@@ -0,0 +1,6 @@
+﻿function getQueryString(field, url) {
+    var href = url ? url : window.location.href;
+    var reg = new RegExp('[?&]' + field + '=([^&#]*)', 'i');
+    var string = reg.exec(href);
+    return string ? string[1] : null;
+};
